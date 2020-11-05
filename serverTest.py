becac2b79078b4e1fa28a16bc87abdddb272703d
jbajlozi <joani28000@gmail.com>
2020-11-04 21:02:42 -0800

    2

--- serverTest.py
+++ serverTest.py
@@ -1,7 +1,6 @@
 # Reminder: This is a comment. The first line imports a default library "socket" into Python.
 # You don’t install this. The second line is initialization to add TCP/IP protocol to the endpoint.
 import socket
-import cv2
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
@@ -18,5 +17,4 @@ while True:
         conn.send("I am SERVER\n".encode())
     conn.close()
     print('client disconnected')
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
